ad855c925d053f030246b0f35130aa868d340035
feeli <feeli@tencent.com>
2020-03-17 16:37:15 +0800

    获取configmap

--- cmd/ServiceAccount.go
+++ cmd/ServiceAccount.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"log"
 	"os"
 	"time"
@@ -11,11 +14,31 @@ import (
 func main() {
 	logfile := flag.String("log","/home/serviceaccount.log","")
 	flag.Parse()
+	//初始化日志
 	file,err := os.OpenFile(*logfile,os.O_APPEND|os.O_CREATE,666);
 	if err != nil {
 		os.Exit(1)
 	}
 	logger := log.New(file,time.Now().String(),log.Ldate|log.Ltime|log.Lshortfile);
-	fmt.Printf(logger)
+
+	//初始化client-go
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for  {
+		cm ,err := clientset.CoreV1().ConfigMaps("tinykube").Get(context.TODO(),"codedns",metav1.GetOptions{})
+		if err != nil{
+			logger.Printf("get configmap fail err = ",err)
+		}
+		logger.Printf(cm.GetName())
+		time.Sleep(1*time.Second)
+	}
+
+
 
 }
