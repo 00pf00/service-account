db263571649b033fe76a947e164af0606153cc1d
feeli <feeli@tencent.com>
2020-03-17 21:21:02 +0800

    update configmap

--- cmd/ServiceAccount.go
+++ cmd/ServiceAccount.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/client-go/rest"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -36,6 +37,7 @@ func main() {
 		fmt.Println("kubernetes.NewForConfig(config)")
 	}
 	for {
+		//获取configmap
 		cm, err := clientset.CoreV1().ConfigMaps("tinykube").Get(context.TODO(), "coredns", metav1.GetOptions{})
 		if err != nil {
 			fmt.Println("get configmap fail")
@@ -46,6 +48,32 @@ func main() {
 		fmt.Println(cm.GetName())
 		for k, v := range cm.Data {
 			fmt.Printf("key = %s value = %s", k, v)
+		}
+		//更新configmap
+		core := cm.Data["Corefile"]
+		start := strings.Index(core, "}")
+		var hosts string
+		if strings.Contains(core, "hosts") {
+			hosts += core[:start+2]
+			d := core[start+2:]
+			e := strings.Index(core[start+2:], "}")
+			hosts += d[e+2:]
+		} else {
+			hosts += core[:start+2]
+			hosts += "    hosts {\n"
+			hosts += "        127.0.0.1     localhost\n"
+			hosts += "        fallthrough\n"
+			hosts += "    }\n"
+			hosts += core[start+2:]
+		}
+		cm.Data["Corefile"] = hosts
+		ucm, err := clientset.CoreV1().ConfigMaps("tinykube").Update(context.TODO(), cm, metav1.UpdateOptions{})
+		if err != nil {
+			fmt.Println("update configmap fail")
+		}
+
+		for k, v := range ucm.Data {
+			fmt.Printf("updated key = %s value = %s", k, v)
 		}
 		eps, err := clientset.CoreV1().Endpoints("tinykube").Get(context.TODO(), "proxycloud", metav1.GetOptions{})
 		if err != nil {
